2aed6eeb9debe98dd2f662b4cd340265f16c453a
Biya Paul <77636493+bpsmartdesign@users.noreply.github.com>
2022-09-17 03:50:29 +0200

    Initial commit

---
+++ cypress.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  e2e: {
+    baseUrl: 'http://localhost:3333',
+    chromeWebSecurity: false,
+    specPattern: 'cypress/e2e/**/*.spec.*',
+    supportFile: false,
+  },
+
+  component: {
+    devServer: {
+      framework: 'vue',
+      bundler: 'vite',
+    },
+  },
+})
---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+useHead({
+  title: 'Think-dev Web starter',
+  meta: [
+    { name: 'description', content: 'Opinionated Vite Starter Template for think-dev web applications' },
+    {
+      name: 'theme-color',
+      content: computed(() => isDark.value ? '#00aba9' : '#ffffff'),
+    },
+  ],
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
