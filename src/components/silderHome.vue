80629549fd132e17b26952b7c74ef4e075925310
hunterbrightdesign <hunterbrightdesign@gmail.com>
2022-10-02 11:01:18 +0100

    add anime for text sildeer

--- src/components/silderHome.vue
+++ src/components/silderHome.vue
@@ -2,7 +2,6 @@
 const props = defineProps<{
   initial: number
 }>()
-
 const { count, inc, dec } = useCounter(props.initial)
 </script>
 
@@ -11,14 +10,21 @@ const { count, inc, dec } = useCounter(props.initial)
     <div class="bg--img">
       <img src="/silder2.jpeg" alt="bg home" w-full h-max>
     </div>
-    <div class="bg-all-header-home"></div>
+    <div class="bg-all-header-home" />
     <div class="content--silder flex flex-col justify-center">
       <div>
-        <p text-xl font-bold mb-5 > Welcome to best it solutions </p>
-        <h1 mb-5 font-bold text-6xl >Provide it solutions and services</h1>
-        <a href="" class="btn--submit mb-5 px-5 text-lg py-3">learn more </a>
+        <div class="content--anime--welcome">
+          <p text-xl font-bold mb-5 class="anime--welcome">
+            Welcome to best it solutions
+          </p>
+        </div>
+        <div class="content--anime--service">
+          <h1 mb-10 font-bold text-6xl class="anime--service">
+            Provide it solutions and services
+          </h1>
+          <a href="" class="btn--submit px-5 text-lg py-3 anime--welcome anime--service">learn more </a>
+        </div>
       </div>
-      <div></div>
     </div>
   </div>
   <div>
@@ -54,7 +60,6 @@ const { count, inc, dec } = useCounter(props.initial)
   z-index: 3;
   text-align: center;
   color: #fff;
-
   width: 100%;
 }
 .btn--submit{
@@ -63,5 +68,44 @@ const { count, inc, dec } = useCounter(props.initial)
   background-color: $icolor ;
   height: 40px;
   width: 50%;
+}
+.anime--welcome{
+  animation: slidein 3s ;
+
+}
+.content--anime--welcome{
+  height: 40px;
+  overflow: hidden;
+}
+.content--anime--service{
+  height: 180px;
+  overflow: hidden;
+}
+.anime--service{
+  animation: slidein2 3s ;
+
+}
+@keyframes slidein {
+  from {
+    // height: 1000px;
+    margin-top: -50px;
+  }
+
+  to {
+    // margin-left: 0%;
+    // width: 100%;
+  }
+}
+@keyframes slidein2 {
+  from {
+    // margin-right: 100%;
+    // width: 300%;
+    margin-top: 150px;
+  }
+
+  to {
+    // margin-right: 0%;
+    // width: 100%;
+  }
 }
 </style>
