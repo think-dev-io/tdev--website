91404b8e39669c69f4ad1ef3d0f4082d83c8770f
hunterbrightdesign <hunterbrightdesign@gmail.com>
2022-10-07 17:26:35 +0100

    state page not found

--- src/layouts/404.vue
+++ src/layouts/404.vue
@@ -4,15 +4,24 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <main p="x4 y10" text="center teal-700 dark:gray-200">
-    <div text-4xl>
-      <div i-carbon-warning inline-block />
-    </div>
-    <RouterView />
-    <div>
-      <button btn text-sm m="3 t8" @click="router.back()">
-        {{ t('button.back') }}
-      </button>
+  <div>
+    <Header />
+  </div>
+  <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+    <div class="mt-5 mx-auto text-center opacity-75 dark:opacity-50 text-sm min-h-90">
+      <div text-4xl>
+        <div i-carbon-warning inline-block />
+      </div>
+      <RouterView />
+      <div>
+        <button btn text-sm m="3 t8" @click="router.back()">
+          {{ t('button.back') }}
+        </button>
+      </div>
     </div>
   </main>
+  <div>
+    <Footer />
+  </div>
 </template>
+
